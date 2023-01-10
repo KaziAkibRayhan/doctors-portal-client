64636ee2d3685e816f1417003276ae23295254a7
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-10 14:14:53 +0600

    project setup and install react-router-dom

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layouts/Main/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/login", element: <Login></Login> },
+    ],
+  },
+]);
