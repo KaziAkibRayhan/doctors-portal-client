b4ea85e0aa52117c4c5d380f303e1249ff08b470
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-10 21:44:16 +0600

    Create simple Top Banner Using Hero component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { routes } from "./Routes/Routes/Routes";
 
 const App = () => {
   return (
-    <div>
+    <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={routes}></RouterProvider>
     </div>
   );
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Chair from "../../../assets/images/chair.png";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="hero banner-img">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={Chair} alt="" className="w-1/2 rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary  bg-gradient-to-r from-secondary to-primary text-white">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
