c159d48c327aae6d69929a417db5d54cf8b43898
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-12 10:22:33 +0600

    appointment chair

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main/Main";
+import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 
@@ -9,6 +10,7 @@ export const routes = createBrowserRouter([
     element: <Main></Main>,
     children: [
       { path: "/", element: <Home></Home> },
+      { path: "/appointment", element: <Appointment></Appointment>},
       { path: "/login", element: <Login></Login> },
     ],
   },
