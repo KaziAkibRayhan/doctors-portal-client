47ea79d2e211b85e36203db2652d5ebd0bc4e887
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-20 19:17:15 +0600

    Upload image to image hosting server imgbb and get image url

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,11 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import Loading from '../../Shared/Loading/Loading';
 
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors } } = useForm()
+    const imageHostKey = process.env.REACT_APP_imgbb_key
+    const { data: specialties, isLoading } = useQuery({
+        queryKey: ['specialty'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/appointmentSpecialty')
+            const data = await res.json()
+            return data;
+        }
+    })
+
     const handleAddDoctor = (data) => {
-        console.log(data);
+        const image = data.image[0]
+        const formData = new FormData()
+        formData.append('image', image)
+        const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`
+        fetch(url, {
+            method: "POST",
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imageData => {
+                if (imageData.success) {
+                    console.log(imageData.data.url);
+                }
+            }).catch(error => console.log(error))
     }
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div className='w-96 p-7'>
             <h3 className="text-3xl">Add A Doctor</h3>
@@ -44,11 +74,28 @@ const AddDoctor = () => {
                     <label className="label">
                         <span className="label-text">Specialty</span>
                     </label>
-                    <select className="select select-bordered w-full max-w-xs">
-                        <option disabled selected>Pick a Specialty</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
+                    <select {...register('specialty')} className="select input-bordered w-full max-w-xs">
+                        {
+                            specialties?.map(specialty => <option
+                                key={specialty._id} value={specialty.name}
+                            >{specialty.name}</option>)
+                        }
                     </select>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
+                    <input
+                        {...register("image", { required: true })}
+                        type="file"
+                        className="input input-bordered w-full max-w-xs"
+                    />
+                    {errors.image && (
+                        <span className="text-red-500 font-bold">
+                            This photo field is required
+                        </span>
+                    )}
                 </div>
                 <input
                     type="submit"
